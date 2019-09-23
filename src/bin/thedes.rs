1509d5dbf39be83d0b6556c6e197047f22908034
brunoczim <brunoczim@gmail.com>
2019-09-22 22:15:56 -0300

    added proper panic handling

--- src/bin/thedes.rs
+++ src/bin/thedes.rs
@@ -1,7 +1,19 @@
-use std::process;
+use backtrace::Backtrace;
+use std::{fs::File, io::Write, panic, process};
 use thedes::backend::Termion;
 
+fn handle_panic(info: &panic::PanicInfo) {
+    let mut file =
+        File::create("thedes_crash.txt").expect("Couldn't create log");
+    write!(file, "{}\n\n", info).expect("Couldn't write log");
+
+    let backtrace = Backtrace::new();
+    write!(file, "{:?}", backtrace).expect("Couldn't write log");
+}
+
 fn main() {
+    panic::set_hook(Box::new(handle_panic));
+
     if let Err(e) = thedes::game_main::<Termion>() {
         eprintln!("{}", e);
         process::exit(-1);
