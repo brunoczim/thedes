fc1ab210a7b98922f73afa25004589de301f43b0
brunoczim <brunoczim@gmail.com>
2023-10-16 22:57:29 -0300

    app stub

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use std::{net::SocketAddr, process};
+
+use clap::{Parser, Subcommand};
+use libthedes::{error::Result, server::Server};
+use tokio_util::sync::CancellationToken;
+
+#[derive(Debug, Clone, Parser)]
+struct Cli {
+    #[command(subcommand)]
+    cmd: Command,
+}
+
+#[derive(Debug, Clone, Subcommand)]
+enum Command {
+    Launch,
+    Serve {
+        #[arg(short = 'a', long = "bind-addr")]
+        bind_addr: SocketAddr,
+    },
+}
+
+async fn try_main(cli: Cli) -> Result<()> {
+    match cli.cmd {
+        Command::Launch => (),
+        Command::Serve { bind_addr } => {
+            let server =
+                Server::new(bind_addr, CancellationToken::new()).await?;
+            server.run().await?;
+        },
+    }
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    let cli = Cli::parse();
+    if let Err(error) = try_main(cli).await {
+        eprintln!("{}", error);
+        process::exit(1);
+    }
+}
