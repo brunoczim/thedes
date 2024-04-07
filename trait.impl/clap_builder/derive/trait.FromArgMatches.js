a544ea1747ac6f1d5d72ac850d73bd5b055a42cb
brunoczim <brunoczim@users.noreply.github.com>
2024-04-07 13:01:39 +0000

    deploy: 91ac5a5c4584ed027f4fe2de2b35625cbfd78f7c

--- trait.impl/clap_builder/derive/trait.FromArgMatches.js
+++ trait.impl/clap_builder/derive/trait.FromArgMatches.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
 "clap":[],
 "clap_builder":[],
-"thedes":[["impl <a class=\"trait\" href=\"clap_builder/derive/trait.FromArgMatches.html\" title=\"trait clap_builder::derive::FromArgMatches\">FromArgMatches</a> for <a class=\"enum\" href=\"thedes/enum.Command.html\" title=\"enum thedes::Command\">Command</a>"],["impl <a class=\"trait\" href=\"clap_builder/derive/trait.FromArgMatches.html\" title=\"trait clap_builder::derive::FromArgMatches\">FromArgMatches</a> for <a class=\"struct\" href=\"thedes/struct.Cli.html\" title=\"struct thedes::Cli\">Cli</a>"]]
+"thedes":[["impl <a class=\"trait\" href=\"clap_builder/derive/trait.FromArgMatches.html\" title=\"trait clap_builder::derive::FromArgMatches\">FromArgMatches</a> for <a class=\"struct\" href=\"thedes/struct.Cli.html\" title=\"struct thedes::Cli\">Cli</a>"],["impl <a class=\"trait\" href=\"clap_builder/derive/trait.FromArgMatches.html\" title=\"trait clap_builder::derive::FromArgMatches\">FromArgMatches</a> for <a class=\"enum\" href=\"thedes/enum.Command.html\" title=\"enum thedes::Command\">Command</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
