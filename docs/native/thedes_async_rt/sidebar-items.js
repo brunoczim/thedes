519c1b5cc522f2630614aa14f8767f9fb1009cc0
brunoczim <brunoczim@users.noreply.github.com>
2025-03-14 01:10:20 +0000

    deploy: 250052bde51988c2753b227889dbbd82a5ee95f9

--- docs/native/thedes_async_rt/sidebar-items.js
+++ docs/native/thedes_async_rt/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"attr":["dynamic_async_trait","static_async_trait"],"mod":["extensions","sync","task","time"],"type":["PanicPayload"]};
+window.SIDEBAR_ITEMS = {"attr":["dynamic_async_trait","static_async_trait"],"macro":["dynamically_scoped"],"mod":["extensions","local","sync","task","time"],"type":["PanicPayload"]};
---
+++ docs/wasm/thedes_async_rt/local/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["AsyncScopeGuard","DynScoped","ScopeGuard"],"trait":["LocalScopeExt"]};
