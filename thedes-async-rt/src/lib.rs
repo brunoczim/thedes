ef66f5631663dada63770895896e538dbe50960b
brunoczim <brunoczim@gmail.com>
2025-03-13 22:06:27 -0300

    add dynamic scope

--- thedes-async-rt/src/lib.rs
+++ thedes-async-rt/src/lib.rs
@@ -10,6 +10,9 @@ use native as backend;
 #[cfg(target_family = "wasm")]
 use wasm as backend;
 
+#[macro_use]
+mod macros;
+
 #[cfg(not(target_family = "wasm"))]
 mod native;
 
@@ -20,5 +23,6 @@ pub mod extensions;
 pub mod task;
 pub mod time;
 pub mod sync;
+pub mod local;
 
 pub type PanicPayload = Box<dyn Any + Send + 'static>;
