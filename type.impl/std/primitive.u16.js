9e107ccb697f8d00cacafd1d8c1f7b6483421db6
brunoczim <brunoczim@users.noreply.github.com>
2025-03-30 17:55:05 +0000

    deploy: 18a801c789c74768af17b9a2bdb6a890164c8e55

--- type.impl/std/primitive.u16.js
+++ type.impl/std/primitive.u16.js
@@ -1,9 +1,9 @@
 (function() {
-    var type_impls = Object.fromEntries([["serde",[]],["thedes_tui",[]]]);
+    var type_impls = Object.fromEntries([["serde",[]],["thedes_tui_core",[]]]);
     if (window.register_type_impls) {
         window.register_type_impls(type_impls);
     } else {
         window.pending_type_impls = type_impls;
     }
 })()
-//{"start":55,"fragment_lengths":[12,18]}
+//{"start":55,"fragment_lengths":[12,23]}
