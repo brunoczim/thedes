6b9cbeeaa405ebc1b196f8d1378c73cc7ee23728
brunoczim <brunoczim@gmail.com>
2025-04-04 00:13:43 -0300

    create thedes-app

--- thedes-bin/src/main.rs
+++ thedes-bin/src/main.rs
@@ -10,14 +10,6 @@ use std::{
 };
 
 use chrono::{Datelike, Timelike};
-use thedes_tui::core::{
-    color::{BasicColor, ColorPair},
-    event::{Event, Key, KeyEvent},
-    geometry::CoordPair,
-    mutation::{MutationExt, Set},
-    screen,
-    tile::{MutateColors, MutateGrapheme},
-};
 use thiserror::Error;
 use tokio::runtime;
 use tracing::level_filters::LevelFilter;
@@ -58,102 +50,14 @@ enum ProgramError {
     #[error("Failed to start asynchronous runtime")]
     AsyncRuntime(#[source] io::Error),
     #[error(transparent)]
-    TuiApp(#[from] TuiAppError),
+    TuiApp(#[from] thedes_app::Error),
     #[error(transparent)]
     TuiRuntime(#[from] thedes_tui::core::runtime::Error),
 }
 
-#[derive(Debug, Error)]
-enum TuiAppError {
-    #[error("Failed to render TUI")]
-    RenderText(
-        #[from]
-        #[source]
-        thedes_tui::text::Error,
-    ),
-    #[error("Failed to interact with screen canvas")]
-    CanvasFlush(
-        #[from]
-        #[source]
-        thedes_tui::core::screen::FlushError,
-    ),
-}
-
-async fn tui_main(mut app: thedes_tui::core::App) -> Result<(), TuiAppError> {
-    let mut timer = app.timer.new_participant();
-
-    let min = CoordPair { y: 1, x: 0 };
-    let max = app.canvas.size() - 1;
-    let mut curr = min + (min + max) / 2;
-
-    'main: loop {
-        app.canvas
-            .queue([screen::Command::ClearScreen(BasicColor::Black.into())]);
-        thedes_tui::text::inline(
-            &mut app,
-            CoordPair { y: 0, x: 0 },
-            "Hello, World! Q/Esc to quit, HJKL to move",
-            ColorPair {
-                foreground: BasicColor::Black.into(),
-                background: BasicColor::LightGreen.into(),
-            },
-        )?;
-        app.canvas.queue([screen::Command::new_mutation(
-            curr,
-            MutateGrapheme(Set('O'.into())).then(MutateColors(Set(
-                ColorPair {
-                    foreground: BasicColor::White.into(),
-                    background: BasicColor::Black.into(),
-                },
-            ))),
-        )]);
-        if app.canvas.flush().is_err() {
-            tracing::info!("Screen command receiver disconnected");
-            break;
-        }
-
-        let Ok(events) = app.events.read_until_now() else {
-            tracing::info!("Event sender disconnected");
-            break;
-        };
-        for event in events {
-            let Event::Key(KeyEvent {
-                alt: false,
-                ctrl: false,
-                shift: false,
-                main_key,
-            }) = event
-            else {
-                continue;
-            };
-
-            match main_key {
-                Key::Char('q') | Key::Char('Q') | Key::Esc => break 'main,
-                Key::Char('h') | Key::Char('H') => {
-                    curr.x = curr.x.saturating_sub(1).max(min.x).min(max.x);
-                },
-                Key::Char('j') | Key::Char('J') => {
-                    curr.y = curr.y.saturating_add(1).max(min.y).min(max.y);
-                },
-                Key::Char('k') | Key::Char('K') => {
-                    curr.y = curr.y.saturating_sub(1).max(min.y).min(max.y);
-                },
-                Key::Char('l') | Key::Char('L') => {
-                    curr.x = curr.x.saturating_add(1).max(min.x).min(max.x);
-                },
-                _ => (),
-            }
-        }
-
-        timer.tick().await;
-    }
-
-    Ok(())
-}
-
 async fn async_runtime_main() -> Result<(), ProgramError> {
     let config = thedes_tui::core::runtime::Config::new();
-    let runtime_future = config.run(tui_main);
+    let runtime_future = config.run(thedes_app::root);
     runtime_future.await??;
     Ok(())
 }
@@ -218,7 +122,6 @@ fn setup_panic_handler() {
         tracing::error!("{}\n", info);
         let backtrace = Backtrace::capture();
         tracing::error!("backtrace:\n{}\n", backtrace);
-        // thedes_tui::panic::emergency_restore();
         eprintln!("{}", info);
         eprintln!("backtrace:\n{}\n", backtrace);
     }));
