ca5866d6ac6b6356b4605914398720e8439936f0
brunoczim <brunoczim@gmail.com>
2025-04-06 16:13:47 -0300

    separate TUI cancel (#114)

---
+++ thedes-tui/src/cancellability.rs
@@ -0,0 +1,63 @@
+pub trait Cancellation<I> {
+    type Output;
+
+    fn is_cancellable(&self) -> bool;
+
+    fn is_cancelling(&self) -> bool;
+
+    fn set_cancelling(&mut self, is_it: bool);
+
+    fn make_output(&self, item: I) -> Self::Output;
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Default)]
+pub struct NonCancelllable;
+
+impl<I> Cancellation<I> for NonCancelllable {
+    type Output = I;
+
+    fn is_cancellable(&self) -> bool {
+        false
+    }
+
+    fn is_cancelling(&self) -> bool {
+        false
+    }
+
+    fn set_cancelling(&mut self, _is_it: bool) {}
+
+    fn make_output(&self, item: I) -> Self::Output {
+        item
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Default)]
+pub struct Cancellable {
+    is_cancelling: bool,
+}
+
+impl Cancellable {
+    pub fn new(initial_cancelling: bool) -> Self {
+        Self { is_cancelling: initial_cancelling }
+    }
+}
+
+impl<I> Cancellation<I> for Cancellable {
+    type Output = Option<I>;
+
+    fn is_cancellable(&self) -> bool {
+        true
+    }
+
+    fn is_cancelling(&self) -> bool {
+        self.is_cancelling
+    }
+
+    fn set_cancelling(&mut self, is_it: bool) {
+        self.is_cancelling = is_it;
+    }
+
+    fn make_output(&self, item: I) -> Self::Output {
+        self.is_cancelling.then_some(item)
+    }
+}
--- thedes-tui/src/lib.rs
+++ thedes-tui/src/lib.rs
@@ -2,4 +2,5 @@ pub use thedes_tui_core as core;
 
 pub mod text;
 pub mod key_bindings;
+pub mod cancellability;
 pub mod menu;
