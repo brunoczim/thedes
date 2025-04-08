f1ba68d1d15ec0c065c21cc7b87a8889e112134a
brunoczim <brunoczim@users.noreply.github.com>
2025-04-08 02:02:00 +0000

    deploy: 3a8feb59cbe8665cc2d1f34e509b71baaf4c9822

---
+++ thedes_tui/info/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["Command","Error"],"fn":["default_key_bindings"],"struct":["Info","Style"],"type":["KeyBindingMap"]};
--- thedes_tui/sidebar-items.js
+++ thedes_tui/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["cancellability","input","key_bindings","menu","text"]};
+window.SIDEBAR_ITEMS = {"mod":["cancellability","info","input","key_bindings","menu","text"]};
